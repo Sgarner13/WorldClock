6176569f9a1a1fe38a6f247ccb9626ec8856d1dd
Sgarner13 <141655757+Sgarner13@users.noreply.github.com>
2023-09-29 14:14:01 +0100

    Added JS to add times and dates relevant to each place.  Removed unnecessary HTML.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,35 @@
+setInterval(function () {
+  let newYorkElement = document.querySelector("#newYork");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/Moncton");
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd, Do MMMM YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    `h:mm:ss [<small>]A[</small>]`
+  );
 
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd, Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    `h:mm:ss [<small>]A[</small>]`
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("dddd, Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    `h:mm:ss [<small>]A[</small>]`
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do MMMM YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format(`h:mm:ss [<small>]A[</small>]`);
+}, 1000);
